91db4efe85afb8d5f1b02579edd4f0e1b8d5b450
BurritoMan46 <32967462+BurritoMan46@users.noreply.github.com>
2018-02-02 16:25:18 +0000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -2,7 +2,13 @@
 
 namespace BurritoMan46/SocialCore;
 
-use pocketmine/plugin/PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase
+use pocketmine\utils\TextFormat;
 
 class SocialCore extends PluginBase{
   
@@ -12,3 +18,8 @@ class SocialCore extends PluginBase{
          
         public function onDisable(){
                 $this->getLogger()->info("onDisable() has been called!");
+          
+          public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+	if(strtolower($command->getName()) === "gkit"){
+		// Execute logic
+		return true;
