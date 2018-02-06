e155e4138e914fcaa5a6a0984197fe1c89da4998
BurritoMan46 <32967462+BurritoMan46@users.noreply.github.com>
2018-02-06 14:38:11 +0000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -2,18 +2,14 @@
 
 namespace BurritoMan46/SocialCore;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\event\player\PlayerJoinEvent;
-
 use pocketmine\event\player\PlayerQuitEvent;
-
 use pocketmine\Player;
-
 use pocketmine\Server;
-
 use pocketmine\event\Listener;
-
 use pocketmine\utils\TextFormat as C;;
 
 class SocialCore extends PluginBase implements Listener{
@@ -28,3 +24,14 @@ class SocialCore extends PluginBase implements Listener{
                     $this->getLogger()->info("SocialCore Disabled!");
           }
 }
+	  public function onJoin(PlayerJoinEvent $event){
+  		 $player = $event->getPlayer();
+   		$name = $player->getName();
+   		$this->getServer()->broadcastMessage(C::GREEN."$name Joined AversionPE! Awesome!");
+	}
+}
+          public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
+}
+
+          if($cmd->getName() == "gkit"){ //In this case, we will make the command "/gkit"
+                $sender->sendMessage("Gkit");
